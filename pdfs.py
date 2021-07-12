40035bf3b457e1bc0184798e91e44d1a8ddcf246
Chris <cjmugs@gmail.com>
2021-07-12 16:04:04 -0500

    updates

--- pdfs.py
+++ pdfs.py
@@ -4,4 +4,8 @@ import PyPDF2
 with open('C:/Gits/pdf/dummy.pdf', 'rb') as file:
     reader =  PyPDF2.PdfFileReader(file)
     page =  reader.getPage(0)
-    print(page.rotateCounterClockwise(90))
+    page.rotateCounterClockwise(90)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+    with open('crooked.pdf', 'wb') as new_file:
+        writer.write(new_file)
