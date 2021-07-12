d304ddba5ca306756c2997e11badac1f8343e96c
Chris <cjmugs@gmail.com>
2021-07-12 16:15:44 -0500

    updates

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,3 +1,11 @@
 import PyPDF2
 import sys
 
+# grabs all of the arguements after the filename
+inputs = sys.argv[1:]
+
+def pdf_combine(pdf_list):
+    for pdf in pdf_list:
+        print(pdf)
+        
+pdf_combine(inputs)
