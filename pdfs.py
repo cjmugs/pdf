b193bf89b02e7c1fc1e4f41fa5d2c155244d1158
Chris <cjmugs@gmail.com>
2021-07-12 10:25:24 -0500

    updates

--- pdfs.py
+++ pdfs.py
@@ -3,4 +3,5 @@ import PyPDF2
 # This is how to open a file object
 with open('C:/Gits/pdf/dummy.pdf', 'rb') as file:
     reader =  PyPDF2.PdfFileReader(file)
-    print(reader.numPages)
+    page =  reader.getPage(0)
+    print(page.rotateCounterClockwise(90))
